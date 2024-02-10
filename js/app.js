96b8971087f0a2a263316fd6c7571c19c50087de
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-02-10 14:24:59 +0700

    sws

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $(".banner-slider").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    // autoplay: true,
+    autoplay: true,
     // arrows: false,
-    // autoplaySpeed: 2000,
+    autoplaySpeed: 2000,
     fade: true
     // dots: true
 })
